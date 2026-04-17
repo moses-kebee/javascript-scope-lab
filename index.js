002791ce178791adb2f30f94abc1abe76adbf285
moses-kebee <mosesnyamweya1@gmail.com>
2026-04-17 16:44:33 +0300

    modify

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-// Global variables
-const burgers = ['Hamburger', 'Cheeseburger'];  // Changed to const
-let featuredDrink = 'Strawberry Milkshake';     // Stays as let
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
 
-// Function with function-scoped variable
 function addBurger() {
-    const newBurger = 'Flatburger';  // Changed to const
+    const newBurger = 'Flatburger';
     burgers.push(newBurger);
 }
 
-// Block-scoped variable inside if(true)
 if (true) {
-    const anotherNewBurger = 'Maple Bacon Burger';  // Changed to const
+    const anotherNewBurger = 'Maple Bacon Burger';
     burgers.push(anotherNewBurger);
 }
 
-// Function to change featured drink
 function changeFeaturedDrink() {
-    featuredDrink = 'The JavaShake';  // Fixed the string
+    featuredDrink = 'The JavaShake';
 }
-
-// Optional: Call functions if needed for testing
-addBurger();  // Call to add Flatburger
-
-console.log('Burgers menu:', burgers);
-console.log('Featured drink:', featuredDrink);
