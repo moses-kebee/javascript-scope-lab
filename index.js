6b57e05168f4db0f55bbe90cfe288ceac190923a
moses-kebee <mosesnyamweya1@gmail.com>
2026-04-17 16:26:26 +0300

    Complete Flatburger Restaurant lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+// Global variables
+let burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function with function-scoped variable
+function addBurger() {
+    let newBurger = 'Flatburger';
+    burgers.push(newBurger);
+}
+addBurger();  
+
+// Block-scoped variable  if(true)
+if (true) {
+    let anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+}
+
+// Function to change featured drink
+function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+}
+
+console.log("Burgers menu:", burgers);
+console.log("Featured drink:", featuredDrink);
